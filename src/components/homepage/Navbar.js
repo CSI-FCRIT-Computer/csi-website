8f6c944c3b935aa0568bf3502be41f680dbad44e
Harshit Kandpal <harshitheroh5@gmail.com>
2023-09-20 09:23:14 +0530

    added all pages and routes to them

--- src/components/homepage/Navbar.js
+++ src/components/homepage/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 import "./Navbar.css";
 
 function Navbar() {
@@ -6,11 +8,11 @@ function Navbar() {
     <div className="navbar">
       <span className="navbar__brand">LOGO</span>
       <ul className="navbar__links" type="none">
-        <li>HOME</li>
-        <li>ABOUT US</li>
-        <li> EVENTS</li>
-        <li>SPONSORS</li>
-        <li>CONTACT US</li>
+        <Link to="/" className="no-underline"><li>HOME</li></Link>
+        <Link to="/AboutUs" className="no-underline"><li>ABOUT US</li></Link>
+        <Link to="/Events" className="no-underline"><li>EVENTS</li></Link>
+        <Link to="/Sponsors" className="no-underline"><li>SPONSORS</li></Link>
+        <Link to="/ContactUs" className="no-underline"><li>CONTACT US</li></Link>
       </ul>
     </div>
   );
