8190b0727b9b2629fe967b2860d967995eeccba8
Harshit Kandpal <harshitheroh5@gmail.com>
2023-09-23 04:47:57 +0530

    improved text

--- src/components/eventsPage/Card.js
+++ src/components/eventsPage/Card.js
@@ -9,9 +9,6 @@ const Card = ({ image, title, tagLine, content, location, date, time }) => {
   return (
     <div className="card__body">
         <div className="card__img"><img src={image} alt={title}/></div>
-        <div className='preview'>
-            <p>{tagLine}</p>
-        </div>
         <div className="hidden">
           <div className="card__title">{title}</div>
           <div className="card__description"><p>{content}</p></div>
