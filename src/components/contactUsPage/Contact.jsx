41c8049c2489b90ccee6623f93fe22c9f89ff941
Savio Dias <diassavio629@gmail.com>
2023-11-06 18:14:31 +0000

    updated contact form

--- src/components/contactUsPage/Contact.jsx
+++ src/components/contactUsPage/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "CSI Team",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "csi.info@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        process.env.EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
